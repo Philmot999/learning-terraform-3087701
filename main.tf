dd0c718745b5b216f3c7717e8eaf1668a160e680
Philmot999 <philippe.mottin@skynet.be>
2024-04-10 15:49:24 +0200

    mod

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = "sg-075b1c782b4a567fa"
 }
 
 resource "aws_security_group_rule" "blog_https_in" {
@@ -53,7 +53,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = "sg-075b1c782b4a567fa"
 }
 
 resource "aws_security_group_rule" "blog_everything_out" {
@@ -63,5 +63,5 @@ resource "aws_security_group_rule" "blog_everything_out" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = "aws_security_group.blog.id"
+  security_group_id = "sg-075b1c782b4a567fa"
 }
