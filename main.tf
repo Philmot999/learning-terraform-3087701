d6db0c8289b3293be133c34f4210553dc46ae14d
Philmot999 <philippe.mottin@skynet.be>
2024-04-10 15:47:24 +0200

    mod

--- main.tf
+++ main.tf
@@ -36,29 +36,32 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
+  type        = "ingress"
   from_port   = 80
   to_port     = 80
-  ip_protocol    = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = "aws_security_group.blog.id"
 }
 
-resource "aws_vpc_security_group_ingress_rule" "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
+  type        = "ingress"
   from_port   = 443
   to_port     = 443
-  ip_protocol    = "tcp"
-  cidr_ipv4 = "0.0.0.0/0"
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = "aws_security_group.blog.id"
 }
 
-resource "aws_vpc_security_group_egress_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
   from_port   = 0
   to_port     = 0
-  ip_protocol    = "-1"
-  cidr_ipv4 = "0.0.0.0/0"
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = "aws_security_group.blog.id"
 }
