1083a8969077858d4bf940829af6ea00d8087811
Philmot999 <philippe.mottin@skynet.be>
2024-04-10 15:31:08 +0200

    modig

--- main.tf
+++ main.tf
@@ -40,17 +40,16 @@ resource "aws_vpc_security_group_ingress_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   ip_protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_ipv4 = "0.0.0.0/0"
 
   security_group_id = "aws_security_group.blog.id"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "blog_https_in" {
-  type        = "ingress"
   from_port   = 443
   to_port     = 443
   ip_protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_ipv4 = "0.0.0.0/0"
 
   security_group_id = "aws_security_group.blog.id"
 }
@@ -58,8 +57,8 @@ resource "aws_vpc_security_group_ingress_rule" "blog_https_in" {
 resource "aws_vpc_security_group_egress_rule" "blog_everything_out" {
   from_port   = 0
   to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  ip_protocol    = "-1"
+  cidr_ipv4 = "0.0.0.0/0"
 
   security_group_id = "aws_security_group.blog.id"
 }
